59474124660eca22d9ac808616406fd74f9bb8ec
ohbyeongmin <fbottlem@gmail.com>
2023-08-30 00:55:45 +0900

    zkpAcc Contract

---
+++ contracts/zkpAccContracts/child/ChildZKPVerify.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Idenrifier: MIT
+
+pragma solidity 0.8.19;
+
+import "../../interfaces/child/IStateReceiver.sol";
+import "../../interfaces/IStateSender.sol";
+
+contract ChildZKPVerify is IStateReceiver {
+    IStateSender public l2StateSender;
+    address public rootZKPVerifyContract;
+    mapping(uint256 => bool) public verification;
+    uint256 public count;
+
+    constructor(address l2StateSenderAddress, address rootZKPVerifyContractAddress) {
+        l2StateSender = IStateSender(l2StateSenderAddress);
+        rootZKPVerifyContract = rootZKPVerifyContractAddress;
+    }
+
+    function onStateReceive(uint256 /* id */, address sender, bytes calldata data) external {
+        bool proof = abi.decode(data, (bool));
+        verify(proof);
+    }
+
+    function verify(bool proof) public {
+        verification[count] = !proof;
+        finishedVerify(count);
+        count++;
+    }
+
+    function finishedVerify(uint256 verificationNum) public {
+        l2StateSender.syncState(rootZKPVerifyContract, abi.encode(verification[verificationNum]));
+    }
+}
---
+++ contracts/zkpAccContracts/root/RootZKPVerify.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import "../../interfaces/IStateSender.sol";
+import "../../interfaces/root/IL2StateReceiver.sol";
+
+contract RootZKPVerify is IL2StateReceiver {
+    IStateSender public stateSender;
+    address public childZKPVerifyContract;
+
+    event FinishedVerification(bool indexed verification);
+
+    constructor(address stateSenderAddress, address childZKPVerifyContractAddress) {
+        stateSender = IStateSender(stateSenderAddress);
+        childZKPVerifyContract = childZKPVerifyContractAddress;
+    }
+
+    function verify(bool proof) public {
+        stateSender.syncState(childZKPVerifyContract, abi.encode(proof));
+    }
+
+    function FinishedVerify(bool verification) public {
+        emit FinishedVerification(verification);
+    }
+
+    function onL2StateReceive(uint256 id, address sender, bytes calldata data) external {
+        bool verification = abi.decode(data, (bool));
+        FinishedVerify(verification);
+    }
+}
